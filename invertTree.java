9ba31a36471313fdd4cf215f9af8f6c35cc8b1e1
Rashi Bansal <rashibansal79518@gmail.com>
2021-02-20 19:58:38 +0530

    Invert Tree / Mirror Tree
    https://leetcode.com/problems/invert-binary-tree/

---
+++ invertTree.java
@@ -0,0 +1,63 @@
+/*
+226. Invert Binary Tree
+Easy
+
+4754
+
+71
+
+Add to List
+
+Share
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        TreeNode temp;
+        if (root==null) return root;
+        invertTree(root.left);
+        invertTree(root.right);
+        temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        return root;
+        
+    }
+}
